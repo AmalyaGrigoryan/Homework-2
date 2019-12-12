f45a5b5b733922586d1a75e7d6e92b051df4e794
AmalyaGrigoryan <58630696+AmalyaGrigoryan@users.noreply.github.com>
2019-12-13 00:04:50 +0300

    Add files via upload
    Exercise 1

---
+++ script.js
@@ -0,0 +1,9 @@
+//1. Given a number. Print “odd” if the number is odd and “even” if it’s even.  
+var number = prompt("Please enter a number") 
+
+if (number % 2){
+    alert("The namber is Odd");
+}else {
+    alert("The number is Even");
+
+}
